e67231ad914ea137766ff45ac192b87c41eb1457
RachaelMSimmonds <36174233+RachaelMSimmonds@users.noreply.github.com>
2020-11-06 13:55:46 -0500

    Update routes.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,3 +1,7 @@
-<Route exact path="/login">
-  <Login />
-</Route>
+import { Switch, Route } from 'react-router';
+import Login from './login';
+export default Router = () => <Switch>
+  <Route exact path="/login">
+    <Login />
+  </Route>
+</Switch>
